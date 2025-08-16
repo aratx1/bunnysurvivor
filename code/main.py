ed3e091f9bc786d57de1fc0233145a2f71070bf0
aratx1 <look4mydreams@hotmail.com>
2025-08-15 23:53:30 -0600

    Se agrega movimiento y colisiones

--- code/main.py
+++ code/main.py
@@ -1,46 +1,50 @@
 from settings import *
 from player import Player
+from sprites import *
 
+from random import randint
 
 class Game:
     def __init__(self):
+        # setup
         pygame.init()
-        self.display_surface = pygame.display.set_mode((ANCHO_VENTADA, ALTO_VENTA))
-        pygame.display.set_caption("Bunny Survivor!")
+        self.display_surface = pygame.display.set_mode((ANCHO_VENTANA, ALTO_VENTANA))
+        pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
 
-        #groups
+        # groups 
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
-        #sprites
-
-        self.player = Player((200, 200), self.all_sprites)
+        # sprites
+        self.player = Player((400,300), self.all_sprites, self.collision_sprites)
+        for i in range(6):
+            x, y = randint(0, ANCHO_VENTANA), randint(0, ALTO_VENTANA)
+            w, h = randint(60,100), randint(50,100)
+            CollisionSprite((x,y), (w,h), (self.all_sprites, self.collision_sprites))
 
     def run(self):
         while self.running:
-            #dt
+            # dt 
             dt = self.clock.tick() / 1000
 
-            #event loop
+            # event loop 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            #update
-
+            # update 
             self.all_sprites.update(dt)
-            #draw
+
+            # draw
+            self.display_surface.fill('black')
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
-
-        
         pygame.quit()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     game = Game()
     game.run()
-
-
 
--- code/settings.py
+++ code/settings.py
@@ -3,5 +3,5 @@ import pygame
 from os.path import join
 from os import walk
 
-ANCHO_VENTADA, ALTO_VENTA =1200, 700
+ANCHO_VENTANA, ALTO_VENTANA =1200, 700
 TILE_SIZE = 64
